a5009cc3eb38155f843b36d94a0c687c404f686d
Danny Baumann <dannybaumann@web.de>
2018-07-18 17:25:32 +0200

    spes: overlay: Enable call recording
    Change-Id: I1b4478b330b13cf5d0fe1ed0d40f43e4b3825f33

--- device.mk
+++ device.mk
@@ -340,7 +340,8 @@ KERNEL_SD_LLVM_SUPPORT := true
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-lineage
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
