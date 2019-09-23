ad54afc604db1f046c7b97797fd56ffdb42d54a1
LuK1337 <priv.luk@gmail.com>
2019-09-23 15:33:35 +0200

    spes: releasetools: Add vbmeta images to the output zip if present
    Change-Id: I18372d2f8f3ab701bd92427c571d7506540f6f05

--- releasetools.py
+++ releasetools.py
@@ -35,4 +35,5 @@ def AddImage(info, basename, dest):
 def OTA_InstallEnd(info):
   AddImage(info, "dtbo.img", "/dev/block/bootdevice/by-name/dtbo")
   AddImage(info, "vbmeta.img", "/dev/block/bootdevice/by-name/vbmeta")
+  AddImage(info, "vbmeta_system.img", "/dev/block/bootdevice/by-name/vbmeta_system")
   return
