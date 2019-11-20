4b2537b85dd70a47e4e28823c34ace871c42f9a1
Rahul Krishna <Rahulkrishna585@gmail.com>
2019-11-20 14:48:41 +0530

    spes: Enable full vndk support
    Change-Id: I1e50e8d66d79200410c19cca7ecd6248c89bd9d3

--- device.mk
+++ device.mk
@@ -495,6 +495,9 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/excluded-input-devices.xml:$(TARGET_COPY_OUT_VENDOR)/etc/excluded-input-devices.xml
 
 # VNDK
+PRODUCT_PACKAGES += \
+    vndk_package
+
 PRODUCT_EXTRA_VNDK_VERSIONS := 30
 
 # WiFi
