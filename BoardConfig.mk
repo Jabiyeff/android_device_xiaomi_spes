90ec97c71f1c9d8efd1d675bfb68e5f964d8579b
Bruno Martins <bgcngm@gmail.com>
2020-10-09 12:19:37 +0100

    spes: power: Adapt extension to new aidl power HAL
    Change-Id: I58ac49957d8d28ad904912986d89ac8db4820ff9

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -201,6 +201,9 @@ TARGET_COPY_OUT_VENDOR := vendor
 # Platform
 TARGET_BOARD_PLATFORM := bengal
 
+# Power
+TARGET_POWERHAL_MODE_EXT := $(DEVICE_PATH)/power/power-mode.cpp
+
 # Properties
 TARGET_ODM_PROP += $(DEVICE_PATH)/odm.prop
 TARGET_PRODUCT_PROP += $(DEVICE_PATH)/product.prop
