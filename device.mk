6435e17b2ff0a2b618b24b9eeb619c9ba7cde3b6
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-02-08 12:30:57 +0100

    spes: Import public libraries list
    * From spes-user 11 MIUI V13.0.5.0.RGCMIXM release-keys
    
    Change-Id: I41725fac378391291e84cca00c16b1661797e22b

--- device.mk
+++ device.mk
@@ -335,6 +335,10 @@ KERNEL_SD_LLVM_SUPPORT := true
 PRODUCT_PACKAGES += \
     android.hardware.power-service-qti
 
+# Public libraries
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
+
 # QMI
 PRODUCT_PACKAGES += \
     libjson \
