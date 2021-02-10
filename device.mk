405d63ddd709f2246598f81245b94fb64525c9b3
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-02-10 21:58:07 +0100

    spes: Build atrace service
    Change-Id: I89b2e6c308fb4ae63680ef25b56bc20a41a2c2a4

--- device.mk
+++ device.mk
@@ -40,6 +40,10 @@ PRODUCT_PACKAGES += \
     checkpoint_gc \
     otapreopt_script
 
+# Atrace
+PRODUCT_PACKAGES += \
+    android.hardware.atrace@1.0-service
+
 # Audio
 PRODUCT_PACKAGES += \
     android.hardware.audio@6.0-impl \
