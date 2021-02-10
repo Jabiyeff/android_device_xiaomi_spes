2e001e06be930054a0c72106e4fc908283c481bd
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-02-10 22:01:44 +0100

    spes: Build health HIDL
    Change-Id: I89c431c7ac87fe878537375cd811606d68f4bb1d

--- device.mk
+++ device.mk
@@ -182,6 +182,11 @@ PRODUCT_PACKAGES += \
     android.hardware.gnss.measurement_corrections@1.1.vendor \
     android.hardware.gnss.visibility_control@1.0.vendor
 
+# Health
+PRODUCT_PACKAGES += \
+    android.hardware.health@2.1-impl \
+    android.hardware.health@2.1-service
+
 # Enable incremental FS feature
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.incremental.enable=1
