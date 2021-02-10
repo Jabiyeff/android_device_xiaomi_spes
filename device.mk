1f5eb4299904135bc394599fc8ebff230a155f6e
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-02-10 22:03:39 +0100

    spes: Build netutils-wrapper-1.0
    Change-Id: I30ac498fc3b5ca431b7cdf1989562d0d1ad0a464

--- device.mk
+++ device.mk
@@ -245,6 +245,10 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video_le.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video_le.xml
 
+# Net
+PRODUCT_PACKAGES += \
+    netutils-wrapper-1.0
+
 # RRO configuration
 TARGET_USES_RRO := true
 
