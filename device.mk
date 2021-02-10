4c8d025f6f5884847093ace12a91b18b6a6f3443
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-02-10 22:10:19 +0100

    spes: Build Wi-Fi stack
    Change-Id: I2e99bb95cbcdb2fdb652e20bb1e9e55b53cab2eb

--- device.mk
+++ device.mk
@@ -349,3 +349,15 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.crypto.volume.filenames_mode = "aes-256-cts" \
     ro.crypto.allow_encrypt_override = true
+
+# WiFi
+PRODUCT_PACKAGES += \
+    android.hardware.wifi@1.0-service \
+    hostapd \
+    libwpa_client \
+    libwifi-hal-ctrl \
+    libwifi-hal-qcom \
+    vendor.qti.hardware.wifi.hostapd@1.2.vendor \
+    vendor.qti.hardware.wifi.supplicant@2.2.vendor \
+    wpa_supplicant \
+    wpa_supplicant.conf
