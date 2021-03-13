8fa1671ea0727b0fa156c04d3fce14d7ba2032a8
daniml3 <daniel@danielml.dev>
2021-03-13 16:03:00 +0100

    spes: releasetools: explicitly print the image to patch
    Signed-off-by: daniml3 <daniel@danielml.dev>
    Change-Id: I56f3f058e8db77967dc1a59bb8fccf86ee3c3559

--- releasetools.py
+++ releasetools.py
@@ -29,10 +29,10 @@ def AddImage(info, basename, dest):
   name = basename
   data = info.input_zip.read("IMAGES/" + basename)
   common.ZipWriteStr(info.output_zip, name, data)
+  info.script.Print("Patching %s" % name.replace(".img", ""))
   info.script.AppendExtra('package_extract_file("%s", "%s");' % (name, dest))
 
 def OTA_InstallEnd(info):
-  info.script.Print("Patching firmware images...")
   AddImage(info, "dtbo.img", "/dev/block/bootdevice/by-name/dtbo")
   AddImage(info, "vbmeta.img", "/dev/block/bootdevice/by-name/vbmeta")
   return
