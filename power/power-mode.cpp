b8d0312e435cea8c6db8aaac891fc94634b25a4e
Arian <arian.kulmer@web.de>
2021-03-22 23:25:57 +0100

    spes: power: Add support for fts
    Change-Id: I1d767578a9092176dbdb18026189ed4aad621a3e

--- power/power-mode.cpp
+++ power/power-mode.cpp
@@ -37,7 +37,7 @@ int open_ts_input() {
 
                 fd = open(absolute_path, O_RDWR);
                 if (ioctl(fd, EVIOCGNAME(sizeof(name) - 1), &name) > 0) {
-                    if (strcmp(name, "goodix_ts") == 0)
+                    if (strcmp(name, "fts") == 0 || strcmp(name, "goodix_ts") == 0)
                         break;
                 }
 
