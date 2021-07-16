e9577c26fc065a058f050c5ce4ee8f343ebe3d56
Lunarixus <Nathan@lunarixus.dev>
2021-07-16 15:41:02 +0530

    [SQUASH]spes: Remove packages at build time
    Squash of the following:
    
    spes: remove_packages: Rename RemovePackages for source
    
    spes: remove_packages: Switch to platform certificate
    
    Co-authored-by: naveenjohnsonv <14140949+naveenjohnsonv@users.noreply.github.com>
    Co-authored-by: Arturo <rtsanonynousperson@gmail.com>
    Signed-off-by: princejb786 <sam.jbjin@gmail.com>

--- device.mk
+++ device.mk
@@ -408,6 +408,10 @@ PRODUCT_PACKAGES += \
     libvndfwk_detect_jni.qti \
     libvndfwk_detect_jni.qti.vendor
 
+# Remove unwanted packages
+PRODUCT_PACKAGES += \
+    RemovePkgs
+
 # RIL
 PRODUCT_PACKAGES += \
     android.hardware.radio@1.5.vendor \
