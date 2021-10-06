841d7ff0eb0e116e2c558fcc7043164e3cc7e502
Marijn Suijten <marijn.suijten@somainline.org>
2021-10-06 22:44:02 +0200

    spes: Provide android.system.net.netd@1.1 on vendor for netmgrd
    Following the previous commit, starting with Android S most HAL
    libraries are not included in VNDK anymore - they need to be explicitly
    pulled in to `/vendor` for odm blobs to be allowed to access them:
    
        linker  : CANNOT LINK EXECUTABLE "/odm/bin/netmgrd": library "android.system.net.netd@1.1.so" not found: needed by main executable
    
    Signed-off-by: Marijn Suijten <marijn.suijten@somainline.org>
    Change-Id: Ide8adad466a61412a984a46d1562f8f2f6a62be3

--- device.mk
+++ device.mk
@@ -345,8 +345,9 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video_le.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video_le.xml
 
-# Net
+# Network
 PRODUCT_PACKAGES += \
+    android.system.net.netd@1.1.vendor \
     netutils-wrapper-1.0
 
 # NFC
