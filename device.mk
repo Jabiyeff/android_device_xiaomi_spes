c8b1804337e6df8409f03f2410a0c9fcf1cfdaa9
ujjwalkirorimal <ujjwalkirorimal@gmail.com>
2021-10-13 20:50:01 +0530

    spes: Update DRM Clearkey HIDL to v1.4

--- device.mk
+++ device.mk
@@ -245,7 +245,7 @@ PRODUCT_PACKAGES += \
     android.hardware.drm@1.0.vendor \
     android.hardware.drm@1.2.vendor \
     android.hardware.drm@1.3.vendor \
-    android.hardware.drm@1.3-service.clearkey
+    android.hardware.drm@1.4-service.clearkey
 
 # Fastboot
 PRODUCT_PACKAGES += \
