386299d169daca9bfc1b9d9b3540c2727d80c4e3
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-03-27 08:57:24 +0700

    spes: Pull libinit from xiaomi-sm8150
    * Ref: https://github.com/LineageOS/android_device_xiaomi_sm8150-common/commits/lineage-18.1/libinit
    
    * Adapt to spes/spesn
    
    Signed-off-by: boedhack99 <boedhack@gmail.com>

---
+++ libinit/init_xiaomi_spes.cpp
@@ -0,0 +1,44 @@
+/*
+ * Copyright (C) 2021 The LineageOS Project
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <libinit_dalvik_heap.h>
+#include <libinit_variant.h>
+
+#include "vendor_init.h"
+
+static const variant_info_t spes_info = {
+    .hwc_value = "",
+    .sku_value = "",
+
+    .brand = "Redmi",
+    .device = "spes",
+    .marketname = "Redmi Note 11",
+    .model = "2201117TG",
+
+    .nfc = false,
+};
+
+static const variant_info_t spesn_info = {
+    .hwc_value = "",
+    .sku_value = "",
+
+    .brand = "Redmi",
+    .device = "spesn",
+    .marketname = "Redmi Note 11",
+    .model = "2201117TY",
+
+    .nfc = true,
+};
+
+static const std::vector<variant_info_t> variants = {
+    spes_info,
+    spesn_info,
+};
+
+void vendor_load_properties() {
+    search_variant(variants);
+    set_dalvik_heap();
+}
