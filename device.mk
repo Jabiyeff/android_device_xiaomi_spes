4b7fefed9140bf28271a6fd7860b70af72a9d5e7
boedhack99 <boedhack@gmail.com>
2022-04-07 20:14:47 +0700

    spes: Generate OTA POSTINSTALL configs

--- device.mk
+++ device.mk
@@ -25,13 +25,20 @@ BOARD_SHIPPING_API_LEVEL := 30
 
 # A/B
 AB_OTA_POSTINSTALL_CONFIG += \
-RUN_POSTINSTALL_vendor=true \
-POSTINSTALL_PATH_vendor=bin/checkpoint_gc \
-FILESYSTEM_TYPE_vendor=ext4 \
-POSTINSTALL_OPTIONAL_vendor=true
+    RUN_POSTINSTALL_system=true \
+    POSTINSTALL_PATH_system=system/bin/otapreopt_script \
+    FILESYSTEM_TYPE_system=ext4 \
+    POSTINSTALL_OPTIONAL_system=true
+
+AB_OTA_POSTINSTALL_CONFIG += \
+    RUN_POSTINSTALL_vendor=true \
+    POSTINSTALL_PATH_vendor=bin/checkpoint_gc \
+    FILESYSTEM_TYPE_vendor=ext4 \
+    POSTINSTALL_OPTIONAL_vendor=true
 
 PRODUCT_PACKAGES += \
-    checkpoint_gc
+    checkpoint_gc \
+    otapreopt_script
 
 # Audio
 PRODUCT_PACKAGES += \
