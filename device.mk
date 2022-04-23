082e4fe112b1bea579b1cd12007bfef8bef1c7ac
Jabiyeff <cebiyevanar@gmail.com>
2022-04-24 00:08:02 +0400

    spes: Build QTI Thermal 2.0 HAL
    Signed-off-by: Jabiyeff <cebiyevanar@gmail.com>

--- device.mk
+++ device.mk
@@ -457,6 +457,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_BOOT_JARS += \
     telephony-ext
 
+# Thermal
+PRODUCT_PACKAGES += \
+    android.hardware.thermal@2.0-service.qti
+
 # Update Engine
 PRODUCT_PACKAGES += \
     update_engine \
