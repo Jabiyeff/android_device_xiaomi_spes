24aa32002369ff1e73d7b0679dfb11610b5c79a5
Jabiyeff <cebiyevanar@gmail.com>
2022-04-30 01:44:23 +0400

    spes: overlay: Import display cutout from SPESGlobal V13.0.9.0
    Signed-off-by: Jabiyeff <cebiyevanar@gmail.com>

--- device.mk
+++ device.mk
@@ -377,8 +377,7 @@ PRODUCT_PACKAGES += \
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-nad
+    $(LOCAL_PATH)/overlay
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
