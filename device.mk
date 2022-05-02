cbb737137bde1e5bec0a23e6e81d4f6024468fc9
Jabiyeff <cebiyevanar@gmail.com>
2022-05-02 13:45:34 +0400

    Revert "spes: Use gsi_keys instead on developer_gsi_keys"
    This reverts commit c71c840295a8b82415bc689066199be2b06e1d35.

--- device.mk
+++ device.mk
@@ -8,7 +8,7 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
 # Include GSI
-$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/developer_gsi_keys.mk)
 
 # Setup dalvik vm configs
 $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
