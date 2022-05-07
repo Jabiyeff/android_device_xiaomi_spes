dc9a283604f0e0608242e4d8d130fb2a3edf0c5b
KakatkarAkshay <akshayakakatkaraa@gmail.com>
2022-05-08 01:22:59 +0400

    spes: Import sunfish powerhint.json from android-12.1.0_r5
    Change-Id: I552870c130ceaac4d20322acf6d1da22ad5017ec
    Signed-off-by: Jabiyeff <cebiyevanar@gmail.com>

--- device.mk
+++ device.mk
@@ -386,6 +386,9 @@ PRODUCT_SOONG_NAMESPACES += \
     hardware/google/interfaces \
     hardware/google/pixel
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
+
 # Public libraries
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
