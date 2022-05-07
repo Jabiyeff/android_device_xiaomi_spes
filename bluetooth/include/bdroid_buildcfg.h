6d6ff4f7c7cb1eaff92107e033bbb12d9857bbc5
Pranav Vashi <neobuddy89@gmail.com>
2022-05-07 12:29:34 +0400

    spes: Switch to QCOM BT
    Change-Id: I07fde33f5dfce4f9c727b340684bddf4b51369c9
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Jabiyeff <cebiyevanar@gmail.com>

--- bluetooth/include/bdroid_buildcfg.h
+++ bluetooth/include/bdroid_buildcfg.h
@@ -25,9 +25,10 @@
 
 // Disables read remote device feature
 #define MAX_ACL_CONNECTIONS   16
-#define MAX_L2CAP_CHANNELS    16
+#define MAX_L2CAP_CHANNELS    32
 #define BLE_VND_INCLUDED   TRUE
+#define GATT_MAX_PHY_CHANNEL  10
 
 /* Increasing SEPs to 12 from 6 to support SHO/MCast i.e. two streams per codec */
-#define AVDT_NUM_SEPS 12
+#define AVDT_NUM_SEPS 35
 #endif
