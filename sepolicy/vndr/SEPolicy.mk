90de65278cd11c3fb3148bf592cd35c8332349ad
Jabiyeff <cebiyevanar@gmail.com>
2022-05-13 15:54:59 +0400

    spes: sepolicy: vndr: Squash adapt for bengal
    * Cleanup unused policies
    * Adapt makefiles and paths for device tree location
    
    Signed-off-by: Jabiyeff <cebiyevanar@gmail.com>

--- sepolicy/vndr/SEPolicy.mk
+++ sepolicy/vndr/SEPolicy.mk
@@ -1,5 +1,5 @@
 # Board specific SELinux policy variable definitions
-SEPOLICY_PATH:= device/qcom/sepolicy_vndr
+SEPOLICY_PATH:= device/xiaomi/spes/sepolicy/vndr
 QSSI_SEPOLICY_PATH:= device/qcom/sepolicy
 SYS_ATTR_PROJECT_PATH := $(TOP)/device/qcom/sepolicy/generic/public/attribute
 SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS := \
@@ -32,48 +32,27 @@ PRODUCT_PRIVATE_SEPOLICY_DIRS := \
     $(QSSI_SEPOLICY_PATH)/generic/product/private \
     $(QSSI_SEPOLICY_PATH)/qva/product/private
 
-ifeq (,$(filter sdm845 sdm710, $(TARGET_BOARD_PLATFORM)))
-    BOARD_VENDOR_SEPOLICY_DIRS := \
-       $(BOARD_VENDOR_SEPOLICY_DIRS) \
-       $(SEPOLICY_PATH) \
-       $(SEPOLICY_PATH)/generic/vendor/common \
-       $(SEPOLICY_PATH)/generic/vendor/common/attribute \
-       $(SEPOLICY_PATH)/qva/vendor/ssg \
-       $(SEPOLICY_PATH)/qva/vendor/common
+BOARD_VENDOR_SEPOLICY_DIRS := \
+   $(BOARD_VENDOR_SEPOLICY_DIRS) \
+   $(SEPOLICY_PATH) \
+   $(SEPOLICY_PATH)/generic/vendor/common \
+   $(SEPOLICY_PATH)/generic/vendor/common/attribute \
+   $(SEPOLICY_PATH)/qva/vendor/ssg \
+   $(SEPOLICY_PATH)/qva/vendor/common
 
-    ifeq ($(TARGET_SEPOLICY_DIR),)
-      BOARD_VENDOR_SEPOLICY_DIRS += $(SEPOLICY_PATH)/generic/vendor/$(TARGET_BOARD_PLATFORM)
-      BOARD_VENDOR_SEPOLICY_DIRS += $(SEPOLICY_PATH)/qva/vendor/$(TARGET_BOARD_PLATFORM)
-    else
-      BOARD_VENDOR_SEPOLICY_DIRS += $(SEPOLICY_PATH)/generic/vendor/$(TARGET_SEPOLICY_DIR)
-      BOARD_VENDOR_SEPOLICY_DIRS += $(SEPOLICY_PATH)/qva/vendor/$(TARGET_SEPOLICY_DIR)
-    endif
-
-    ifneq (,$(filter userdebug eng, $(TARGET_BUILD_VARIANT)))
-      BOARD_VENDOR_SEPOLICY_DIRS += $(SEPOLICY_PATH)/generic/vendor/test
-      BOARD_VENDOR_SEPOLICY_DIRS += $(SEPOLICY_PATH)/qva/vendor/test
-      BOARD_VENDOR_SEPOLICY_DIRS += $(SEPOLICY_PATH)/qva/vendor/test/sysmonapp
-      BOARD_VENDOR_SEPOLICY_DIRS += $(SEPOLICY_PATH)/qva/vendor/test/mst_test_app
-    endif
+ifeq ($(TARGET_SEPOLICY_DIR),)
+    BOARD_VENDOR_SEPOLICY_DIRS += $(SEPOLICY_PATH)/generic/vendor/$(TARGET_BOARD_PLATFORM)
+    BOARD_VENDOR_SEPOLICY_DIRS += $(SEPOLICY_PATH)/qva/vendor/$(TARGET_BOARD_PLATFORM)
+else
+    BOARD_VENDOR_SEPOLICY_DIRS += $(SEPOLICY_PATH)/generic/vendor/$(TARGET_SEPOLICY_DIR)
+    BOARD_VENDOR_SEPOLICY_DIRS += $(SEPOLICY_PATH)/qva/vendor/$(TARGET_SEPOLICY_DIR)
 endif
 
-ifneq (,$(filter sdm845 sdm710, $(TARGET_BOARD_PLATFORM)))
-    BOARD_VENDOR_SEPOLICY_DIRS := \
-                 $(BOARD_VENDOR_SEPOLICY_DIRS) \
-                 $(SEPOLICY_PATH) \
-                 $(SEPOLICY_PATH)/legacy/vendor/ssg \
-                 $(SEPOLICY_PATH)/legacy/vendor/common
-
-    ifeq ($(TARGET_SEPOLICY_DIR),)
-      BOARD_VENDOR_SEPOLICY_DIRS += $(SEPOLICY_PATH)/legacy/vendor/$(TARGET_BOARD_PLATFORM)
-    else
-      BOARD_VENDOR_SEPOLICY_DIRS += $(SEPOLICY_PATH)/legacy/vendor/$(TARGET_SEPOLICY_DIR)
-    endif
-    ifneq (,$(filter userdebug eng, $(TARGET_BUILD_VARIANT)))
-      BOARD_VENDOR_SEPOLICY_DIRS += $(SEPOLICY_PATH)/legacy/vendor/test
-      BOARD_VENDOR_SEPOLICY_DIRS += $(SEPOLICY_PATH)/legacy/vendor/test/sysmonapp
-      BOARD_VENDOR_SEPOLICY_DIRS += $(SEPOLICY_PATH)/legacy/vendor/test/mst_test_app
-    endif
+ifneq (,$(filter userdebug eng, $(TARGET_BUILD_VARIANT)))
+    BOARD_VENDOR_SEPOLICY_DIRS += $(SEPOLICY_PATH)/generic/vendor/test
+    BOARD_VENDOR_SEPOLICY_DIRS += $(SEPOLICY_PATH)/qva/vendor/test
+    BOARD_VENDOR_SEPOLICY_DIRS += $(SEPOLICY_PATH)/qva/vendor/test/sysmonapp
+    BOARD_VENDOR_SEPOLICY_DIRS += $(SEPOLICY_PATH)/qva/vendor/test/mst_test_app
 endif
 
 -include device/lineage/sepolicy/qcom/sepolicy.mk
