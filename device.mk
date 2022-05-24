63ec2323243f00a9e7bff52f60791e839cd8e93f
Jabiyeff <cebiyevanar@gmail.com>
2022-05-25 02:02:19 +0400

    spes: Build QTI Health IMPL if its available
    * Another way, use AOSP Health IMPL
    
    Signed-off-by: Jabiyeff <cebiyevanar@gmail.com>

--- device.mk
+++ device.mk
@@ -277,8 +277,14 @@ PRODUCT_PACKAGES += \
     android.hardware.gatekeeper@1.0.vendor
 
 # Health
+ifneq ($(wildcard vendor/qcom/opensource/healthd-ext/Android.bp),)
 PRODUCT_PACKAGES += \
-    android.hardware.health@2.1-impl \
+    android.hardware.health@2.1-impl-qti
+else
+PRODUCT_PACKAGES += \
+    android.hardware.health@2.1-impl
+endif
+PRODUCT_PACKAGES += \
     android.hardware.health@2.1-service
 
 # HIDL
