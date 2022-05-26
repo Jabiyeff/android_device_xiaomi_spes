f29a904f80fcc13fc8ed74ea13aa907e323860c7
Jabiyeff <cebiyevanar@gmail.com>
2022-05-26 15:19:54 +0400

    Revert "spes: Ship cgroups.json and task_profiles.json in vendor"
    * Our device first api level is 30 (Android 11), no need these
    
    This reverts commit bcc6de6c926df85ad3332433c8b1982ec657e455.
    
    Signed-off-by: Jabiyeff <cebiyevanar@gmail.com>

--- device.mk
+++ device.mk
@@ -179,11 +179,6 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.camera.device@1.0.vendor \
     vendor.qti.hardware.camera.postproc@1.0.vendor
 
-# Cgroup and task_profiles
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/cgroups.json:$(TARGET_COPY_OUT_VENDOR)/etc/cgroups.json \
-    $(LOCAL_PATH)/configs/task_profiles.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json
-
 # Component overrides
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/component-overrides.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sysconfig/component-overrides.xml \
