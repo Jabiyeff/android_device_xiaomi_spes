11fed634090c7f3fd0bef625862a8175ba7d55ac
Alexander Winkowski <dereference23@outlook.com>
2022-05-28 21:55:35 +0400

    spes: Update sensors service to 2.1
    Change-Id: Ie417407c36929cf34ecbdd28feb1cab996fe1f30
    Signed-off-by: Jabiyeff <cebiyevanar@gmail.com>

--- device.mk
+++ device.mk
@@ -453,7 +453,7 @@ PRODUCT_PACKAGES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
-    android.hardware.sensors@2.0-service.multihal \
+    android.hardware.sensors@2.1-service.multihal \
     libsensorndkbridge
 
 # Soong namespaces
