c8eb9be2362753655b92cf752413799ebbc6a29f
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-06-25 23:55:45 +0400

    spes: Move to common Xiaomi fingerprint HIDL
    Change-Id: I6c29a831e985ee440c92a98aad8d33f25b9e5b2d
    Signed-off-by: Jabiyeff <cebiyevanar@gmail.com>

--- device.mk
+++ device.mk
@@ -254,7 +254,7 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.1-service.spes
+    android.hardware.biometrics.fingerprint@2.3-service.xiaomi
 
 # FM
 PRODUCT_PACKAGES += \
