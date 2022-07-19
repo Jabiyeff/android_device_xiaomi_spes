d719104eeecfa27ef4bca41074856bbfc41fa921
Arian <arian.kulmer@web.de>
2022-07-19 10:52:57 +0400

    spes: Add KeyHandler to handle fingerprint key event
    etylix: adapt for spes with key code 353
    
    Change-Id: Ia2de25da301930c3e61a81cb0033b63009404dd1
    Signed-off-by: Anar Jabiyev <cebiyevanar@gmail.com>

--- device.mk
+++ device.mk
@@ -306,6 +306,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.keymaster@4.1.vendor
 
+# KeyHandler
+PRODUCT_PACKAGES += \
+    KeyHandler
+
 # Lights
 PRODUCT_PACKAGES += \
     android.hardware.lights-service.spes
