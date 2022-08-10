4bd71ed7ecc73f94094efca7d1d1cf8ae64fea5b
Jabiyeff <cebiyevanar@gmail.com>
2022-08-10 22:00:58 +0400

    spes: Remove intree HALs
    * Switch to source based HALs
    
    Signed-off-by: Jabiyeff <cebiyevanar@gmail.com>

--- hals/display/init/Android.mk
+++
@@ -1,9 +0,0 @@
-LOCAL_PATH:= $(call my-dir)
-include $(CLEAR_VARS)
-LOCAL_MODULE       := init.qti.display_boot.sh
-LOCAL_MODULE_TAGS  := optional
-LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := init.qti.display_boot.sh
-LOCAL_INIT_RC      := init.qti.display_boot.rc
-LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_EXECUTABLES)
-include $(BUILD_PREBUILT)
