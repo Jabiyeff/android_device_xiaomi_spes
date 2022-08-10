6d71b4be8b1b4d1c0d470359e46000097c1fc0bc
Jabiyeff <cebiyevanar@gmail.com>
2022-08-10 22:00:58 +0400

    spes: Remove intree HALs
    * Switch to source based HALs
    
    Signed-off-by: Jabiyeff <cebiyevanar@gmail.com>

--- device.mk
+++ device.mk
@@ -456,7 +456,6 @@ PRODUCT_PACKAGES += \
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
-    $(LOCAL_PATH)/hals/display/display-commonsys-intf \
     hardware/xiaomi
 
 # Telephony
--- hals/audio/mm-audio/aenc-amrnb/Android.mk
+++
@@ -1,8 +0,0 @@
-ifneq ($(filter arm aarch64 arm64, $(TARGET_ARCH)),)
-
-
-AENC_AMR_PATH:= $(call my-dir)
-
-include $(AENC_AMR_PATH)/qdsp6/Android.mk
-
-endif
