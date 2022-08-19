81eaf96e63d0e84ce0239c93aeb4d961ac020a62
Anar Jabiyev <cebiyevanar@gmail.com>
2022-08-19 08:52:19 +0400

    spes: Remove deprecated audio.a2dp.default blobs
    Signed-off-by: Anar Jabiyev <cebiyevanar@gmail.com>
    Signed-off-by: Jabiyeff <cebiyevanar@gmail.com>

--- device.mk
+++ device.mk
@@ -104,7 +104,6 @@ PRODUCT_PACKAGES += \
     android.hardware.soundtrigger@2.3-impl
 
 PRODUCT_PACKAGES += \
-    audio.a2dp.default \
     audio.bluetooth.default \
     audio.r_submix.default \
     audio.primary.bengal \
