533e91a68fdb6d09f0e3d1bd069d32cccbb3bb41
Anar Jabiyev <cebiyevanar@gmail.com>
2022-08-19 08:46:40 +0400

    spes: ArrowOS bring up for Android 13
    Signed-off-by: Anar Jabiyev <cebiyevanar@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_spes.mk
+    $(LOCAL_DIR)/arrow_spes.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_spes-user \
-    lineage_spes-userdebug \
-    lineage_spes-eng
+    arrow_spes-user \
+    arrow_spes-userdebug \
+    arrow_spes-eng
