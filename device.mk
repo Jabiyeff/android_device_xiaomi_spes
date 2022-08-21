c7b2458e0e67b340676eeb1c63d44234590f0856
Jabiyeff <cebiyevanar@gmail.com>
2022-08-21 11:11:30 +0400

    Revert "spes: Move to common Xiaomi fingerprint HIDL"
    * Unstable on Android 13
    
    This reverts commit c8eb9be2362753655b92cf752413799ebbc6a29f.
    
    Signed-off-by: Jabiyeff <cebiyevanar@gmail.com>

--- device.mk
+++ device.mk
@@ -237,7 +237,7 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.3-service.xiaomi
+    android.hardware.biometrics.fingerprint@2.1-service.spes
 
 # FM
 PRODUCT_PACKAGES += \
