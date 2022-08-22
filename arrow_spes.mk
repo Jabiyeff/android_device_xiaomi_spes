1fb32fc6fedc8e68079db8951c4ee4bfb447a115
Jabiyeff <cebiyevanar@gmail.com>
2022-08-22 12:33:31 +0400

    spes: Import GApps for ArrowOS
    Signed-off-by: Jabiyeff <cebiyevanar@gmail.com>

--- arrow_spes.mk
+++ arrow_spes.mk
@@ -15,7 +15,7 @@ $(call inherit-product, device/xiaomi/spes/device.mk)
 $(call inherit-product, vendor/arrow/config/common.mk)
 
 # Additions
-$(call inherit-product-if-exists, vendor/gapps/config.mk)
+$(call inherit-product-if-exists, vendor/gapps/common/common-vendor.mk)
 $(call inherit-product-if-exists, vendor/prebuilt-apps/config.mk)
 
 # Camera
