e39a0009d1bf940f0168cd2f666f9604b8b2fcb3
Jabiyeff <cebiyevanar@gmail.com>
2022-10-15 18:59:37 +0400

    spes: Update GApps repository location
    Signed-off-by: Jabiyeff <cebiyevanar@gmail.com>

--- lineage_spes.mk
+++ lineage_spes.mk
@@ -15,7 +15,7 @@ $(call inherit-product, device/xiaomi/spes/device.mk)
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Additions
-$(call inherit-product-if-exists, vendor/gapps/common/common-vendor.mk)
+$(call inherit-product-if-exists, vendor/gapps/common.mk)
 $(call inherit-product-if-exists, vendor/prebuilt-apps/config.mk)
 
 # Camera
