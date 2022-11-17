6c362a09c3329fa8463d73461e3194880310b652
enweazudaniel <daniel.enweazu@student.aul.edu.ng>
2022-11-17 11:09:58 +0100

    spes: superior-ify

--- lineage_spes.mk
+++ lineage_spes.mk
@@ -12,7 +12,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/xiaomi/spes/device.mk)
 
 # Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/superior/config/common.mk)
 
 # Additions
 $(call inherit-product-if-exists, vendor/gapps/common.mk)
@@ -22,7 +22,7 @@ $(call inherit-product-if-exists, vendor/prebuilt-apps/config.mk)
 $(call inherit-product-if-exists, vendor/miuicamera/config.mk)
 
 # Product Specifics
-PRODUCT_NAME := lineage_spes
+PRODUCT_NAME := superior_spes
 PRODUCT_DEVICE := spes
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := Redmi Note 11
@@ -36,3 +36,5 @@ DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-lineage
 # LiveDisplay
 PRODUCT_PACKAGES += \
     vendor.lineage.livedisplay@2.0-service-sdm
+    
+TARGET_BOOT_ANIMATION_RES := 1080
