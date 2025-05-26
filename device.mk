73505e8f8b89561ad52af03b72bee078ac759dd4
Jabiyeff <cebiyevanar@gmail.com>
2025-05-26 11:44:48 +0400

    spes: Remove old WiFi Overlay for prepare new one
    Signed-off-by: Jabiyeff <cebiyevanar@gmail.com>

--- device.mk
+++ device.mk
@@ -523,7 +523,6 @@ PRODUCT_PACKAGES += \
     libwifi-hal-qcom \
     vendor.qti.hardware.wifi.hostapd@1.2.vendor \
     vendor.qti.hardware.wifi.supplicant@2.2.vendor \
-    WifiResCommon \
     wpa_supplicant \
     wpa_supplicant.conf
 
