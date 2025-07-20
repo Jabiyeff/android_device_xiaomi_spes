01d8ff34e2768d554f222a493f40a10045d9018d
muralivijay <muralidharan9845@gmail.com>
2025-07-20 23:32:24 +0400

    spes: init: Don,t override sku value as nfc . if device supports nfc. * it override exiting sku value "k7tn" into "nfc" . so nfc hals failed to start and cannot use k7tn manifests.
    Signed-off-by: Jabiyeff <cebiyevanar@gmail.com>

--- libinit/libinit_variant.cpp
+++ libinit/libinit_variant.cpp
@@ -41,7 +41,4 @@ void set_variant_props(const variant_info_t variant) {
 
         property_override("ro.build.description", fingerprint_to_description(variant.build_fingerprint));
     }
-
-    if (variant.nfc)
-        property_override(SKU_PROP, "nfc");
 }
