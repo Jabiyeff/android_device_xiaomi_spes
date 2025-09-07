af7a1acaff00a4175172cbd7e73496517a07fa68
Jabiyeff <cebiyevanar@gmail.com>
2025-09-07 15:57:40 +0400

    spes: Import device based compatibility matrix for manifest
    Signed-off-by: Jabiyeff <cebiyevanar@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -118,7 +118,8 @@ TARGET_OTA_ASSERT_DEVICE := spes,spesn
 # HIDL
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
-    hardware/xiaomi/vintf/xiaomi_framework_compatibility_matrix.xml
+    hardware/xiaomi/vintf/xiaomi_framework_compatibility_matrix.xml \
+    $(DEVICE_PATH)/device_compatibility_matrix.xml
 DEVICE_MATRIX_FILE += hardware/qcom-caf/common/compatibility_matrix.xml
 DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest.xml
 
