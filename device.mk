265f053eb4d5c186e331e2aa27e3574b29ec7b09
Yumi Yukimura <me.cafebabe@gmail.com>
2025-09-07 12:55:48 +0400

    spes: Switch to QTI Memtrack AIDL HAL
    Signed-off-by: Jabiyeff <cebiyevanar@gmail.com>

--- device.mk
+++ device.mk
@@ -192,9 +192,7 @@ USE_DEX2OAT_DEBUG := false
 PRODUCT_PACKAGES += \
     android.hardware.graphics.mapper@3.0-impl-qti-display \
     android.hardware.graphics.mapper@4.0-impl-qti-display \
-    android.hardware.memtrack@1.0-impl \
-    android.hardware.memtrack@1.0-service \
-    memtrack.bengal
+    vendor.qti.hardware.memtrack-service
 
 PRODUCT_PACKAGES += \
     vendor.display.config@1.14 \
